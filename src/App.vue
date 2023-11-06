88f461db341ea11e4eddf58f98b9c43ff7e3524f
Mans Ramazanov <Mans-606@yandex.ru>
2023-11-06 15:18:27 +0300

    add app.vue and styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header">
+    <div class="search-container">
+      <div class="search">
+        <img
+          src="./img/search-icon.svg"
+          alt="иконка поиска"
+          class="search-icon"
+        />
+        <input class="search-input" type="text" placeholder="Имя персонажа" />
+        <select class="search-select" name="status">
+            <option class="search-select-option" value="">Status</option>
+          <option class="search-select-option" value="alive">Alive</option>
+          <option class="search-select-option" value="dead">Dead</option>
+          <option class="search-select-option" value="unknown">Unknown</option>
+        </select>
+      </div>
+    </div>
+    <img
+      src="./img/Rick_and_Morty.svg.png"
+      alt="логотип Рик и Морти"
+      class="logo"
+    />
+    <h1 class="header-text">Mansur Ramazanov Test</h1>
+  </header>
+<main class="main">
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</main>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("./img/2020-07-rick-and-morty-4k-8k-free-ultra-hd-pictures-backgrounds-images1.jpg");
+  background-size: 100% 100%;
+}
+
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.header-text {
+  color: azure;
+  display: inline-block;
+  font-size: 20px;
+}
+
+.search-input {
+    margin-left: 10px;
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid #444;
+  color: white;
+}
+
+.search-select {
+    margin-left: 10px;
+  background: transparent;
+  border: none;
+  color: white;
+}
+
+.search-select-option {
+    color: black;
+}
+
+.search-container {
+  display: inline-block;
+}
+
+.search-icon {
+  max-width: 30px;
+}
+
+.search {
+  display: flex;
+  align-items: stretch;
+}
+
+.logo {
+  max-width: 200px;
 }
 </style>
